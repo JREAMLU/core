4047b29da9f483d7adad2d81b76066744047ffe2
lujunjie <lujunjie.@plu.cn>
2016-06-02 18:48:58 +0800

    add base64 func

---
+++ base64/base64.go
@@ -0,0 +1,19 @@
+package base64
+
+import (
+	"encoding/base64"
+)
+
+const (
+	base64Table = "123QRSTUabcdVWXYZHijKLAWDCABDstEFGuvwxyzGHIJklmnopqr234560178912"
+)
+
+var coder = base64.NewEncoding(base64Table)
+
+func Base64Encode(src []byte) []byte {
+	return []byte(coder.EncodeToString(src))
+}
+
+func Base64Decode(src []byte) ([]byte, error) {
+	return coder.DecodeString(string(src))
+}
