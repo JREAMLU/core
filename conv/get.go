85e93b750c621d129dbfeffadf2ee131662c0a59
lujunjie <lujunjie.@plu.cn>
2016-06-28 18:39:54 +0800

    get请求参数获取转换类型

--- conv/get.go
+++ conv/get.go
@@ -1,9 +1,31 @@
 package conv
 
 import (
+	"encoding/json"
 	"errors"
+	"net/http"
 	"strconv"
 )
+
+func GetToJson(r *http.Request) string {
+	var get = make(map[string]interface{})
+	r.ParseForm()
+	for k, v := range r.Form {
+		vint64, err := AssignGetInt64(v[0])
+		if err == nil {
+			get[k] = vint64
+			continue
+		}
+		vfloat64, err := AssignGetFloat64(v[0])
+		if err == nil {
+			get[k] = vfloat64
+			continue
+		}
+		get[k] = v[0]
+	}
+	js, _ := json.Marshal(get)
+	return string(js)
+}
 
 func AssignGetInt64(str string) (int64, error) {
 	stri, _ := strconv.ParseInt(str, 10, 64)
