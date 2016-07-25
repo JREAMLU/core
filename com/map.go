039b7133e2a56df9c96668fb35cd91fe5d108f61
jream <ms.08.lu@gmail.com>
2016-07-25 17:28:49 +0800

    update core 1.mergeMap add sync lock

--- com/map.go
+++ com/map.go
@@ -1,10 +1,16 @@
 package com
 
+import "sync"
+
+var mu sync.Mutex
+
 func MapMerge(ms ...map[string]interface{}) map[string]interface{} {
 	var nm = make(map[string]interface{})
 	for _, m := range ms {
 		for k, v := range m {
+			mu.Lock()
 			nm[k] = v
+			mu.Unlock()
 		}
 	}
 	return nm
