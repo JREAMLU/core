b1b626722434c97c360e444ad74ac55c93c27537
jream <ms.08.lu@gmail.com>
2016-07-26 17:45:13 +0800

    update core

--- mq/kafkaproducer.go
+++ mq/kafkaproducer.go
@@ -1,8 +1,6 @@
 package mq
 
 import (
-	"strings"
-
 	"github.com/Shopify/sarama"
 	log "github.com/thinkboy/log4go"
 )
@@ -11,58 +9,49 @@ var (
 	producer sarama.AsyncProducer
 )
 
-func InitKafka(kafkaAddrs []string, quit chan int) (err error) {
+func InitKafka(kafkaAddrs []string) (err error) {
 	config := sarama.NewConfig()
 	config.Producer.RequiredAcks = sarama.NoResponse
 	config.Producer.Partitioner = sarama.NewHashPartitioner
 	config.Producer.Return.Successes = true
 	config.Producer.Return.Errors = true
 	producer, err = sarama.NewAsyncProducer(kafkaAddrs, config)
-	go handleSuccess(quit)
-	go handleError(quit)
+	go handleSuccess(producer)
+	go handleError(producer)
+
 	return
 }
 
-func handleSuccess(quit chan int) {
-	var (
-		pm *sarama.ProducerMessage
-	)
+func handleSuccess(p sarama.AsyncProducer) {
 	for {
-		pm = <-producer.Successes()
+		pm := <-p.Successes()
 		if pm != nil {
 			log.Info("producer message success, partition:%d offset:%d key:%v valus:%s", pm.Partition, pm.Offset, pm.Key, pm.Value)
 		}
-		quit <- 1
 	}
 }
 
-func handleError(quit chan int) {
-	var (
-		err *sarama.ProducerError
-	)
+func handleError(p sarama.AsyncProducer) {
 	for {
-		err = <-producer.Errors()
+		err := <-p.Errors()
 		if err != nil {
 			log.Error("producer message error, partition:%d offset:%d key:%v valus:%s error(%v)", err.Msg.Partition, err.Msg.Offset, err.Msg.Key, err.Msg.Value, err.Err)
 		}
-		quit <- 2
 	}
 }
 
-func pushKafka(topic string, value string) (err error) {
+func PushKafka(topic string, value string) (err error) {
 	producer.Input() <- &sarama.ProducerMessage{Topic: topic, Value: sarama.StringEncoder(value)}
 	return
 }
 
-//KafkaProducerPush 生产
-func KafkaProducerPush(topic string, value string, addr string) {
-	var quit = make(chan int)
-	kafkaAddrs := strings.Split(addr, ",")
-	InitKafka(kafkaAddrs, quit)
-	err := pushKafka(topic, value)
-	if err != nil {
-		log.Info("pushKafka err: ", err)
+func Close() {
+	if producer == nil {
+		return
 	}
-	handleQuit := <-quit
-	log.Info("handleQuit: ", handleQuit)
+
+	err := producer.Close()
+	if err != nil {
+		log.Error("关闭AsyncProducer时发生错误:%s", err.Error())
+	}
 }
