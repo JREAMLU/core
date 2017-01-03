1e043cc756f028cfe01a0a9dff28792d74b59729
jream <ms.08.lu@gmail.com>
2017-01-03 14:37:38 +0800

    update test case

--- jlogs/beelogs_test.go
+++ jlogs/beelogs_test.go
@@ -11,8 +11,43 @@ import (
 func TestLogs(t *testing.T) {
 	Convey("func Logs()", t, func() {
 		Convey("correct", func() {
-			beego.SetLogger(logs.AdapterConn, `{"net":"udp4","addr":":1200"}`)
+			filename := `{"filename":"1.log", "separate":[""], "daily": true, "maxdays": 7}`
+			beego.SetLogger(logs.AdapterMultiFile, filename)
 			beego.Info("info 123")
 		})
 	})
+}
+
+// func TestLogs(t *testing.T) {
+// 	Convey("func Logs()", t, func() {
+// 		Convey("correct", func() {
+// 			beego.SetLogger(logs.AdapterConn, `{"net":"udp4","addr":":1200"}`)
+// 			beego.Info("info 123")
+// 		})
+// 	})
+// }
+
+// func BenchmarkLogs(b *testing.B) {
+// 	filename := `{"filename":"1.log", "separate":[""], "daily": true, "maxdays": 7}`
+// 	beego.SetLogger(logs.AdapterMultiFile, filename)
+// 	b.StopTimer()
+// 	b.StartTimer()
+// 	Convey("bench Logs()", b, func() {
+// 		for i := 0; i < b.N; i++ {
+// 			beego.Info("info 123")
+// 		}
+// 	})
+// 	b.StopTimer()
+// }
+
+func BenchmarkLogs(b *testing.B) {
+	beego.SetLogger(logs.AdapterConn, `{"net":"udp4","addr":":1200"}`)
+	b.StopTimer()
+	b.StartTimer()
+	Convey("bench Logs()", b, func() {
+		for i := 0; i < b.N; i++ {
+			beego.Info("info 123")
+		}
+	})
+	b.StopTimer()
 }
